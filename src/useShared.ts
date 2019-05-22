72075b747e93eb83fdfe28545c76dc15f0c1fdae
ryym <ryym.64@gmail.com>
2019-05-23 08:08:17 +0900

    refactor: Create own useShared file

---
+++ src/useShared.ts
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+import { makeSharedApi } from './makeSharedApi';
+import { SharedContext } from './SharedProvider';
+
+const ERR_MSG_NO_CTX =
+  '[react-hook-shared] Please wrap your component by SharedProvider to use useShared';
+
+export const useShared = (spaceId: Symbol, componentName: string) => {
+  const sharedSpace = useContext(SharedContext);
+  if (sharedSpace == null) {
+    throw new Error(ERR_MSG_NO_CTX);
+  }
+
+  let space = sharedSpace.get(spaceId);
+  if (space == null) {
+    space = {
+      state: {
+        states: [],
+        listeners: {},
+      },
+      reducer: {
+        states: [],
+        listeners: {},
+      },
+      effect: {
+        listenerCount: 0,
+        effects: [],
+      },
+      multiEffects: [],
+    };
+    sharedSpace.set(spaceId, space);
+  }
+
+  const api = makeSharedApi(space, componentName);
+  return api;
+};
